245bde1fdbbdacedaf66e93bce066c3338d3d875
Siya Digra <siyadigra@gmail.com>
2023-04-04 00:27:41 -0500

    Wrote query for games with keyword

--- App_steamgames/backend/routes/games.js
+++ App_steamgames/backend/routes/games.js
@@ -3,5 +3,19 @@ import { db } from "../index.js";
 
 const gameRoutes = express.Router();
 
+// Game search API
+gameRoutes.post("/search", (req, res) => {
+    // Check if game has keyword
+    var gameName = req.body.name;
+    const q1 = `SELECT * FROM Games WHERE name = '%${gameName}%'`;
+    
+    db.query(q1, (err, data) => {
+        if (err) res.status(500).json({message: err.message});
+        else {
+            res.status(200).json({message: 'Games found!', data});
+        }
+    });
+});
+
 
 export default gameRoutes;
