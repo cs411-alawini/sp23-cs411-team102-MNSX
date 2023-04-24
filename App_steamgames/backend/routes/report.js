e8eb749e3ab0b5f5292a0c9579376a92dfbf1756
minghua3 <minghua3@illinois.edu>
2023-04-24 08:52:41 -0500

    touch up

--- App_steamgames/backend/routes/report.js
+++ App_steamgames/backend/routes/report.js
@@ -14,7 +14,7 @@ reportRoutes.post("/generate/genrereport", (req, res) => {
         if (err) return res.status(403).json({message: "Token is invalid!"});
         else {
             var username = userinfo.username;
-            const q = `call steamgames.generate_report('${username}', @TotalSearchNum, @SearchDistinctNum, @MostSearchgameID, @HighRateGenreName, @HighRateGamelist)`;
+            const q = `call steamgames.generate_report('${username}', True)`;
             db.query(q, (err, sqlData) => {
                 if (err) res.status(500).json({message: err.message});
                 else res.status(200).json({message: 'OK', GenreReport: sqlData});
@@ -33,10 +33,11 @@ reportRoutes.post("/generate/gamereport", (req, res) => {
     jwt.verify(token, process.env.SECRET_KEY, (err, userinfo) => {
         if (err) return res.status(403).json({message: "Token is invalid!"});
         else {
-            const q = `select @TotalSearchNum as TotalSearchNum, @SearchDistinctNum as SearchDistinctNum, @MostSearchgameID as MostSearchgameID, @HighRateGenreName as HighRateGenreName, @HighRateGamelist as HighRateGamelist`;
+            var username = userinfo.username;
+            const q = `call steamgames.generate_report('${username}', False)`;
             db.query(q, (err, sqlData) => {
                 if (err) res.status(500).json({message: err.message});
-                else res.status(200).json({message: 'OK', GameReport: sqlData});
+                else res.status(200).json({message: 'OK', GameReport: sqlData[0]});
             });
         }
     });
--- App_steamgames/frontend/src/pages/Register.jsx
+++ App_steamgames/frontend/src/pages/Register.jsx
@@ -13,6 +13,7 @@ const Register = () => {
     });
 
     const [err, setErr] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleChange = (e) => {
         setUserinfo((prev) => ({...prev, [e.target.name]: e.target.value}));
@@ -22,6 +23,7 @@ const Register = () => {
     const {login} = useContext(AuthContext);
 
     const handleClick = async (e) => {
+        setIsLoading(true);
         e.preventDefault();
 
         try {
@@ -33,6 +35,7 @@ const Register = () => {
         } catch (err) {
             setErr(err.response.data.message);
         }
+        setIsLoading(false);
     };
 
 
@@ -45,7 +48,7 @@ const Register = () => {
                     <input type="username" placeholder="Username" name="username" onChange={handleChange} />
                     <input type="password" placeholder="Password" name="password" onChange={handleChange} />
                     {err && <p className="errMessage">{err}</p>}
-                    <button onClick={handleClick}>Sign up</button>
+                    <button onClick={handleClick}>Sign up {isLoading && <i className="fa fa-circle-o-notch fa-spin"></i>}</button>
                 </form>
                 <p>Already have an account? <Link to="/login">Login here!</Link></p>
             </div>
